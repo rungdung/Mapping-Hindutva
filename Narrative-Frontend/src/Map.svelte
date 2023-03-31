bdf0a81797b84447a58ad562320a82d003c2f7e5
Adhavan Sivaraj <adhavan.sivaraj16@gmail.com>
2023-03-31 16:13:13 +0530

    css order change

--- Narrative-Frontend/src/Map.svelte
+++ Narrative-Frontend/src/Map.svelte
@@ -7,8 +7,8 @@
 
 <script>
   import { onMount } from "svelte";
-  import "leaflet/dist/leaflet.js";
   import "leaflet/dist/leaflet.css";
+  import "leaflet/dist/leaflet.js";
   import "leaflet.markercluster";
   import "leaflet.markercluster/dist/MarkerCluster.css";
   import "leaflet.markercluster/dist/MarkerCluster.Default.css";
