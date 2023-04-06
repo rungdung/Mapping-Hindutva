a42ad5f9b5aab9898428c2c1d5d1425de6ac8f92
Adhavan Sivaraj <adhavan.sivaraj16@gmail.com>
2023-04-06 06:20:42 +0530

    Complete remaking to allow svelvet

--- Narrative-Frontend/src/SelectedFeatures.svelte
+++ Narrative-Frontend/src/SelectedFeatures.svelte
@@ -1,57 +1,138 @@
-<!-- SelectedFeaturesList.svelte -->
+<script context="module">
+  export let nodes = [];
+</script>
+
 <script>
+  import "leaflet";
+  import { map } from "./Map.svelte";
+  import Svelvet from "svelvet";
+
   import { selectedFeatures } from "./stores.js";
-  import "leaflet"
-  import {map} from "./Map.svelte";
-  
+  import { onMount } from "svelte";
+
   let highlightLayer = new L.FeatureGroup();
 
-  function zoomToFeature(feature){
+  let edges = [];
+  let id = 1;
+  export let width;
+  export let height
+
+  function zoomToFeature(feature) {
     highlightLayer.clearLayers();
-    console.log("Zooming to feature")
+    console.log("Zooming to feature");
 
     // Add a circle marker to highlight the feature
-    highlightLayer.addLayer(new L.CircleMarker(feature.geometry.coordinates.reverse(), {
+    highlightLayer.addLayer(
+      new L.CircleMarker(feature.geometry.coordinates.reverse(), {
         radius: 30,
         color: "#ff7800",
         weight: 5,
-        opacity: 0.65
-    }));
+        opacity: 0.65,
+      })
+    );
 
-
-    map.flyTo(feature.geometry.coordinates, 8);
+    map.flyTo(feature.geometry.coordinates, 4);
     highlightLayer.addTo(map);
-
   }
+
+  function highlightAllFeatures(feature) {
+    highlightLayer.clearLayers();
+    console.log("Zooming to feature");
+
+    $selectedFeatures.forEach((feature) => {
+      highlightLayer.addLayer(
+        new L.CircleMarker(feature.feature.geometry.coordinates.reverse(), {
+          radius: 30,
+          color: "#ff7800",
+          weight: 5,
+          opacity: 0.9,
+        })
+      );
+    });
+
+    // map.flyTo(feature.geometry.coordinates, 8);
+    highlightLayer.addTo(map);
+  }
+  
+  // update nodes on load
+  const features = $selectedFeatures;
+  features.forEach((feature) => {
+    nodes.push({
+      id: `${id++}`,
+      position: { x: 0, y: 0 },
+      data: { html: String(feature.title) },
+      width: 100,
+      height: 50,
+      clickCallback: () => zoomToFeature(feature.feature),
+    });
+  });
+  // on new addition
+  $: {
+    const features = $selectedFeatures;
+    const lastFeature = features[features.length - 1];
+    if (lastFeature) {
+      nodes.push({
+        id: `${id++}`,
+        position: { x: 0, y: 0 },
+        data: { html: String(lastFeature.title) },
+        width: 100,
+        height: 50,
+        clickCallback: () => zoomToFeature(lastFeature.feature),
+      });
+    }
+  }
+
 </script>
 
+<div id="events-in-focus">
+  {#key $selectedFeatures}
+    <Svelvet
+      nodes={nodes}
+      edges={edges}
+      width={width}
+      height={height}
+      background={true}
+      bgColor={"antiquewhite"}
+      nodeCreate={true}
+      snap={true}
+      editable
+      initialZoom={1}
+      shareable={false}
+    />
+  {/key}
 
-<h2 class="font-semibold text-xl ">Events in focus</h2>
+  {#if nodes.length < 1}
+    <p>No events selected. Please open an event and click, "Add to list"</p>
+  {:else}
+    <button
+      on:click={() => highlightAllFeatures(selectedFeatures)}
+      class="bg-slate-700"
+    >
+      Highlight all events
+    </button>
 
-{#if $selectedFeatures.length > 0}
-  <ul>
-    {#each $selectedFeatures as feature}
-      <li>{@html feature.properties.title}</li>
-      <!-- To zoom to selected feature-->
-      <button on:click={() => zoomToFeature(feature)}
-        class="bg-slate-700">
-        Zoom to event
-      </button>
-    {/each}
-  </ul>
-{:else}
-  <p>No events selected. Please open an event and click, "Add to list"</p>
-{/if}
+    <button
+      on:click={() => {
+        selectedFeatures.set([]);
+        highlightLayer.clearLayers();
+        nodes = [];
+      }}
+      class="bg-slate-700"
+    >
+      Clear all events
+    </button>
+  {/if}
+</div>
 
 <style>
-button {
-  margin-top: 0.2em;
-  size: 1em;
-  padding: 0.2em 0.5em;
-}
+  button {
+    margin-top: 0.2em;
+    size: 1em;
+    padding: 0.2em 0.5em;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 1em;
-}
+  ul {
+    list-style-type: none;
+    padding: 1em;
+  }
 </style>
--- Narrative-Frontend/src/stores.js
+++ Narrative-Frontend/src/stores.js
@@ -1,3 +1,16 @@
 import { writable } from 'svelte/store';
+import { onDestroy } from 'svelte';
+import { onMount } from "svelte";
+import { persisted } from 'svelte-local-storage-store';
+import {get} from 'svelte/store';
+import { nodes } from './SelectedFeatures.svelte'
+  
+const retrieved = localStorage.getItem("storedFeatures");
+const parsed = JSON.parse(retrieved)
+export const selectedFeatures = writable(parsed === null ? [] : parsed)
 
-export const selectedFeatures = writable([]);
+
+selectedFeatures.subscribe(value => {
+    localStorage.setItem("storedFeatures", JSON.stringify(value))
+});
+
