c2d129efe55d6a70404133e1f05dff61cfcb66a9
Adhavan <adhavan.sivaraj16@gmail.com>
2025-04-09 12:15:34 +0530

    Move to pnpm

--- Narrative-Frontend/vite.config.js
+++ Narrative-Frontend/vite.config.js
@@ -3,15 +3,5 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [sveltekit()],
-  build: {
-    target: "es2022",
-  },
-  esbuild: {
-    target: "es2022",
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      target: "es2022",
-    },
-  },
+
 });
